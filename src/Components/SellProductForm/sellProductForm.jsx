e2c7f0be67e75e7c9569815bf9c5c527c9be6db9
mmilton <84526474+murraymilton@users.noreply.github.com>
2021-08-16 19:17:30 +0200

    Added to component for product registration for user

--- src/Components/SellProductForm/sellProductForm.jsx
+++ src/Components/SellProductForm/sellProductForm.jsx
@@ -2,3 +2,34 @@ import axios from 'axios';
 import './sellProductForm.css'
 import React, {useState} from 'react';
 
+const sellProductForm = (props) => {
+    let id;
+    let getAllProducts = props.getAllProducts;
+    let userCategoryId = props.userCategoryId;
+    if(props.currentUser !== undefined) {id = props.currentUser.user.id;
+}
+const sellerInput = {
+    CategoryId: "",
+    Description:"",
+    Name:"",
+    Price: 0,
+    UserId: id
+}
+const[userEntry, setUserEntry] = useState(userInput)
+const[nameEntry, setNameEntry] = useState({})
+const[descriptionEntry, setDescriptionEntry] = useState({})
+const[productPriceEntry, setProductPriceEntry] = useState({})
+}
+
+const addProductEntrySubmission = () => {
+    userEntry.CategoryId = "";
+    let priceProductEntry =
+}
+
+const submitProductEntry = async () =>{
+    let productEntry
+
+}
+return(
+    <React
+)
