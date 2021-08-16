abf7652cebc4a03179ded7213c0572c25cad4c5d
BA_CalderonMorales <tbcm640@gmail.com>
2021-08-16 13:28:09 -0500

    Attempting to pull from main component.

--- src/App.js
+++ src/App.js
@@ -1,35 +1,13 @@
-import axios from 'axios';
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import Home from './Components/Home/home.jsx';
 
 function App() {
-  const [products, setProducts] = useState([]);
-  
-  useEffect (
-    () => {
-      axios.get('https://localhost:44394/api/product/')
-      .then(res => setProducts(res.data))
-    }, [])
   
   return (
     <>
-      <section className="colored-section">
-        {
-          products.map((product) => {
-            return (
-              <React.Fragment key={product.id}>
-                <h1>Details of A Product</h1>
-        
-                <p>Name: {product.name}</p>
-                <p>Price: {product.price}</p>
-                <p>Description: {product.description}</p>
-                <p>Category: {product.category}</p>
-                <p>Price: {product.price}</p>
-                <p>Average Rating: {product.reviews}</p>
-              </React.Fragment>
-            )
-          })
-        }
-      </section>
+      {/* Leads to Login Page */}
+      {/* <Login /> */}
+      <Home /> 
     </>
   );
 }
